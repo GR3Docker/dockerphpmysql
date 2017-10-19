19d3384415e71bb036cf66244f2787711c0cd9ad
brumax27 <tit-max@live.fr>
2017-10-19 23:10:16 +0200

    ajout test bdd

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
 echo "Vous pouvez travailler sur votre application PHP !";
-
-?>
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=mysql;charset=utf8', 'root', 'root');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
